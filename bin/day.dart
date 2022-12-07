fa4ed22c9ea61003fbbc1e8ca9a3b5166730d277
Iain Smith <b099l3@gmail.com>
2022-12-07 08:40:10 +0000

    change to use a cache for the input, as I was on the train

--- bin/day.dart
+++ bin/day.dart
@@ -7,30 +7,39 @@ import 'secrets.dart';
 
 class Day {
   Stream<String> lines = Stream.empty();
-  int year;
 
-  Day(int dayNumber, this.year) {
-    lines = remoteFile(dayNumber);
+  Day(int dayNumber, year) {
+    final dayNumberPadded = dayNumber.toString().padLeft(2, '0');
+    final filePath = './input/$year/day$dayNumberPadded.txt';
 
-    // I was manually copying the input but thats lame.
-    //lines = localFile(dayNumber);
+    if (File(filePath).existsSync()) {
+      lines = localFile(year, dayNumber, filePath);
+    } else {
+      lines = remoteFile(year, dayNumber, filePath);
+    }
   }
 
-  Stream<String> remoteFile(int dayNumber) {
+  Stream<String> remoteFile(int year, int dayNumber, String filePath) {
     final url =
         Uri.parse('https://adventofcode.com/$year/day/$dayNumber/input');
 
+    final file = File(filePath);
+    file.createSync(recursive: true);
     final response = http
         .get(url, headers: {'Cookie': 'session=$code'})
         .asStream()
         .map((response) => response.body)
+        .map((body) {
+          file.writeAsStringSync(body);
+          return body;
+        })
         .transform(LineSplitter());
+
     return response;
   }
 
-  Stream<String> localFile(int dayNumber) {
-    final dayNumberPadded = dayNumber.toString().padLeft(2, '0');
-    return File('./input/day$dayNumberPadded.txt')
+  Stream<String> localFile(int year, int dayNumber, String filePath) {
+    return File(filePath)
         .openRead()
         .transform(utf8.decoder)
         .transform(LineSplitter());
