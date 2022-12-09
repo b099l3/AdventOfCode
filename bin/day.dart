7f8cba6c8615a9d6f4468864407a3dd824782484
Iain Smith <b099l3@gmail.com>
2022-12-09 23:36:33 +0000

    setup testInput override-able parameters to enable reading from the test examples

--- bin/day.dart
+++ bin/day.dart
@@ -5,14 +5,19 @@ import 'package:http/http.dart' as http;
 
 import 'secrets.dart';
 
-class Day {
+abstract class Day {
   Stream<String> lines = Stream.empty();
+
+  String testInput = '';
+  bool runTestInput = false;
 
   Day(int dayNumber, year) {
     final dayNumberPadded = dayNumber.toString().padLeft(2, '0');
     final filePath = './input/$year/day$dayNumberPadded.txt';
 
-    if (File(filePath).existsSync()) {
+    if (runTestInput) {
+      lines = Stream.fromIterable(testInput.split('\n'));
+    } else if (File(filePath).existsSync()) {
       lines = localFile(year, dayNumber, filePath);
     } else {
       lines = remoteFile(year, dayNumber, filePath);
